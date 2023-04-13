275658a02ca539a95f3e4b5233deb2d5475d14f0
Kauan-Cesar <kauancesarpiassetadasilva@gmail.com>
2023-04-13 11:00:59 -0300

    Criados laços enquanto, para e faça

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Laço enquanto - while
+        int contador = 0;
+        while (contador < 10){
+            System.out.println("contador =" + contador);
+            //incremento
+            contador = contador + 1;
+        }
+        //Laço do para faça - for
+        for(int x = 0; x < 10; x++){
+            System.out.println("Contador = " +x);
+        }
+        // Laço faça enquanto - do while
+        int cont = 1;
+        do{
+            //processo
+            System.out.println(cont);
+            cont++; //incremento
+
+        }while(cont <= 10);
+
     }
 }
+
+
+
+
+
+
+
