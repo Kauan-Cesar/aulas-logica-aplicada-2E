09312ce6f0930486231abcdebd886e5199d73c6e
Kauan-Cesar <kauancesarpiassetadasilva@gmail.com>
2023-05-04 11:55:00 -0300

    Aula d introduçâo

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Laço enquanto - while
-        int contador = 0;
-        while (contador < 10){
-            System.out.println("contador =" + contador);
-            //incremento
-            contador = contador + 1;
-        }
-        //Laço do para faça - for
-        for(int x = 0; x < 10; x++){
-            System.out.println("Contador = " +x);
-        }
-        // Laço faça enquanto - do while
-        int cont = 1;
-        do{
-            //processo
-            System.out.println(cont);
-            cont++; //incremento
 
-        }while(cont <= 10);
 
     }
 }
