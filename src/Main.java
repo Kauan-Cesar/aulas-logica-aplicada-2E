604cf001090e350c96565c2edfae73a098cfe64f
Kauan-Cesar <kauancesarpiassetadasilva@gmail.com>
2023-05-25 11:38:53 -0300

    Raulzito Idolo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        //Ala introdutoria de orientacao a objetos
+        Pessoa anao = new Pessoa();
+
+        //Declaracao de objetos
+        Pessoa qualquer;
+
+        //Instanciaçõa do obj
+        qualquer = new Pessoa();
+
+        //Descrição do formarto pessoa qualuer
+        qualquer.nome = "Raul";
+        qualquer.sobrenome = "Zito";
+        qualquer.falar();
+
+
 
 
     }
---
+++ src/Pessoa.java
@@ -0,0 +1,14 @@
+public class Pessoa {
+    //Atributos da pessoa
+    public String nome;
+
+    public String sobrenome;
+    //Métodos de classe
+    public void falar() {
+        System.out.printf("Falei");
+    }
+
+
+
+
+}
